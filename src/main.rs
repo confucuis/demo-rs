736a480af383de8bbd2859975f0fb3afd358654a
confucuis <confucuis@github.com>
2024-04-28 10:22:55 +0800

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 // use echo;
 // use chat;
-use proxy;
+// use proxy;
 
 fn main() {
     // echo::start_server();
     // chat::start_server();
-    proxy::start_server();
+    // proxy::start_server();
+    let p1 = Person { name: "zhangsan" };
+    let p2 = Person { name: "wangerxiao" };
+    who(p1);
+    who(p2);
+}
+
+trait Action {
+    fn run(&self);
+    fn say(&self);
+}
+
+struct Person<'a> {
+    name: &'a str,
+}
+
+impl Action for Person<'_> {
+    fn run(&self) {
+        println!("{} runing...", self.name);
+    }
+    fn say(&self) {
+        println!("{} saying...", self.name);
+    }
+}
+
+fn who(person: Person) {
+    person.run();
+    person.say();
 }
