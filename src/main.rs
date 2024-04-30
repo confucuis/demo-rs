5da2ab2a2b988bb9567cc5c981863bdf024dc185
confucuis <confucuis@github.com>
2024-04-30 16:35:07 +0800

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-// use echo;
-// use chat;
-// use proxy;
-use interface;
+use chat;
+use echo;
+use proxy;
 
 fn main() {
-    // echo::start_server();
-    // chat::start_server();
-    // proxy::start_server();
-
-    let cat = interface::Cat;
-    let dog = interface::Dog;
-    interface::sound_wawa(&cat);
-    interface::sound_meme(&dog);
+    echo::start_server();
+    chat::start_server();
+    proxy::start_server();
 }
