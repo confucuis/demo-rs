93656332ebfb2cee36793ebcb577a8ff7c2c4224
confucuis <confucuis@github.com>
2024-11-05 03:34:38 +0000

    ..

--- combination/src/lib.rs
+++ combination/src/lib.rs
@@ -1,57 +1,57 @@
 trait Primeter {
-  type Item;
-  fn primeter(&self) -> Self::Item;
+    type Item;
+    fn primeter(&self) -> Self::Item;
 }
 
 trait Extend {
-  type Item;
-  fn extend(&self) -> Self::Item;
+    type Item;
+    fn extend(&self) -> Self::Item;
 }
 
 #[derive(Clone)]
 struct Circle {
-  radius: f64,
+    radius: f64,
 }
 
 impl Primeter for Circle {
-  type Item = f64;
-  fn primeter(&self) -> Self::Item {
-    2.0 * 3.14 * self.radius
-  }
+    type Item = f64;
+    fn primeter(&self) -> Self::Item {
+        2.0 * 3.14 * self.radius
+    }
 }
 
 impl Extend for Circle {
-  type Item = f64;
-  fn extend(&self) -> Self::Item {
-    3.14 * self.radius * self.radius
-  }
+    type Item = f64;
+    fn extend(&self) -> Self::Item {
+        3.14 * self.radius * self.radius
+    }
 }
 
 #[derive(Clone)]
 struct Square {
-  side: u64,
+    side: u64,
 }
 
 impl Primeter for Square {
-  type Item = u64;
-  fn primeter(&self) -> Self::Item {
-    4 * self.side
-  }
+    type Item = u64;
+    fn primeter(&self) -> Self::Item {
+        4 * self.side
+    }
 }
 
 impl Extend for Square {
-  type Item = u64;
-  fn extend(&self) -> Self::Item {
-    self.side * self.side
-  }
+    type Item = u64;
+    fn extend(&self) -> Self::Item {
+        self.side * self.side
+    }
 }
 
 fn calculations_primeter<T: Primeter>(shap: &T) -> T::Item {
-  shap.primeter()
+    shap.primeter()
 }
 
 fn calculations_extend<T: Extend>(shap: &T) -> T::Item {
-  shap.extend()
+    shap.extend()
 }
 
 #[cfg(test)]
@@ -60,8 +60,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let circle = Circle{radius: 2.5};
-        let square = Square{side: 4};
+        let circle = Circle { radius: 2.5 };
+        let square = Square { side: 4 };
         let circle_primter = calculations_primeter(&circle);
         let square_primter = calculations_primeter(&square);
         let circle_extend = calculations_extend(&circle);
